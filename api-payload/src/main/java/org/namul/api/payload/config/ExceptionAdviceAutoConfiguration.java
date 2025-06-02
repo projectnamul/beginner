fa305371d5043f6483408a4b77494b0fe1314dcb
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-02 13:40:25 +0900

    Change Registry and Configurer - Change registry contains only handler and ErrorReasonDTO - Change configurer to contains all Exception with ExceptionHandlerRegistry and be able to register and unregister handler

--- api-payload/src/main/java/org/namul/api/payload/config/ExceptionAdviceAutoConfiguration.java
+++ api-payload/src/main/java/org/namul/api/payload/config/ExceptionAdviceAutoConfiguration.java
@@ -1,21 +1,14 @@
 package org.namul.api.payload.config;
 
-import jakarta.validation.ConstraintViolationException;
-import org.namul.api.payload.error.*;
+import org.namul.api.payload.code.dto.ErrorReasonDTO;
+import org.namul.api.payload.error.ExceptionAdvice;
 import org.namul.api.payload.error.configurer.ExceptionAdviceConfigurer;
-import org.namul.api.payload.error.exception.ServerApplicationException;
-import org.namul.api.payload.handler.*;
-import org.namul.api.payload.registry.ExceptionHandlerRegistry;
 import org.namul.api.payload.writer.FailureResponseWriter;
 import org.namul.api.payload.writer.supports.DefaultFailureResponseWriter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
-import java.util.Optional;
 
 @AutoConfiguration
 public class ExceptionAdviceAutoConfiguration {
@@ -27,64 +20,8 @@ public class ExceptionAdviceAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnClass(ConstraintViolationException.class)
-    ConstraintViolationExceptionHandler constraintViolationExceptionHandler(FailureResponseWriter failureResponseWriter) {
-        return new ConstraintViolationExceptionHandler(failureResponseWriter);
-    }
-
-    @Bean
-    @ConditionalOnClass(MethodArgumentNotValidException.class)
-    MethodArgumentNotValidExceptionHandler methodArgumentNotValidExceptionHandler(FailureResponseWriter failureResponseWriter) {
-        return new MethodArgumentNotValidExceptionHandler(failureResponseWriter);
-    }
-
-    @Bean
-    ServerApplicationExceptionHandler serverApplicationExceptionHandler(FailureResponseWriter failureResponseWriter) {
-        return new ServerApplicationExceptionHandler(failureResponseWriter);
-    }
-
-    @Bean
-    GlobalExceptionHandler globalExceptionHandler(FailureResponseWriter failureResponseWriter) {
-        return new GlobalExceptionHandler(failureResponseWriter);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(ExceptionHandlerRegistry.class)
-    ExceptionHandlerRegistry exceptionHandlerRegistry(Optional<ConstraintViolationExceptionHandler> constraintViolationExceptionHandler,
-                                                      Optional<MethodArgumentNotValidExceptionHandler> methodArgumentNotValidExceptionHandler,
-                                                      Optional<ServerApplicationExceptionHandler> serverApplicationExceptionHandler,
-                                                      GlobalExceptionHandler globalExceptionHandler) {
-        ExceptionHandlerRegistry exceptionHandlerRegistry = new ExceptionHandlerRegistry();
-        constraintViolationExceptionHandler.ifPresent(handler -> exceptionHandlerRegistry.addHandler(ConstraintViolationException.class, handler));
-        methodArgumentNotValidExceptionHandler.ifPresent(handler -> exceptionHandlerRegistry.addHandler(MethodArgumentNotValidException.class, handler));
-        serverApplicationExceptionHandler.ifPresent(handler -> exceptionHandlerRegistry.addHandler(ServerApplicationException.class, handler));
-        exceptionHandlerRegistry.addHandler(Exception.class, globalExceptionHandler);
-        return exceptionHandlerRegistry;
-    }
-
-    @Bean
     @ConditionalOnBean(ExceptionAdviceConfigurer.class)
-    @ConditionalOnClass(ConstraintViolationException.class)
-    ConstraintViolationExceptionAdvice constraintViolationExceptionAdvice(ExceptionAdviceConfigurer configurer) {
-        return configurer.getConstraintViolationExceptionAdvice();
-    }
-
-    @Bean
-    @ConditionalOnBean(ExceptionAdviceConfigurer.class)
-    @ConditionalOnClass(MethodArgumentNotValidException.class)
-    MethodArgumentNotValidExceptionAdvice methodArgumentNotValidExceptionAdvice(ExceptionAdviceConfigurer configurer) {
-        return configurer.getMethodArgumentNotValidExceptionAdvice();
-    }
-
-    @Bean
-    @ConditionalOnBean(ExceptionAdviceConfigurer.class)
-    ServerApplicationExceptionAdvice serverApplicationExceptionAdvice(ExceptionAdviceConfigurer configurer) {
-        return configurer.getServerApplicationExceptionAdvice();
-    }
-
-    @Bean
-    @ConditionalOnBean(ExceptionAdviceConfigurer.class)
-    GlobalExceptionAdvice advices(ExceptionAdviceConfigurer configurer) {
-        return configurer.getGlobalExceptionAdvice();
+    <R extends ErrorReasonDTO> ExceptionAdvice<R> exceptionAdvice(ExceptionAdviceConfigurer<R> exceptionAdviceConfigurer) {
+        return new ExceptionAdvice<>(exceptionAdviceConfigurer);
     }
 }
---
+++ api-payload/src/main/java/org/namul/api/payload/error/ExceptionAdviceRegistry.java
@@ -0,0 +1,19 @@
+package org.namul.api.payload.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.namul.api.payload.code.dto.ErrorReasonDTO;
+import org.namul.api.payload.handler.ExceptionAdviceHandler;
+
+/**
+ * The registry contains handler and ErrorReasonDTO
+ * @param <E> The type of exception will be handled by handler
+ * @param <R> The type of ErrorReasonDTO
+ */
+@Getter
+@RequiredArgsConstructor
+public class ExceptionAdviceRegistry<E extends Exception, R extends ErrorReasonDTO> {
+
+    private final ExceptionAdviceHandler<E, R> handler;
+    private final R errorReasonDTO;
+}
