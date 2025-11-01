ec256cad2eb52d65caf4792597a270984c19b0db
Jeongmo Seo <wjdah1402@gmail.com>
2025-11-01 23:00:29 +0900

    Remove unused import

--- api-payload/src/main/java/org/namul/api/payload/response/DefaultResponse.java
+++ api-payload/src/main/java/org/namul/api/payload/response/DefaultResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.namul.api.payload.code.DefaultResponseSuccessCode;
 import org.namul.api.payload.code.dto.supports.DefaultResponseSuccessReasonDTO;
