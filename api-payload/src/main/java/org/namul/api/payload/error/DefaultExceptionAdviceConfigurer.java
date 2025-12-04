806d511553f09da21cf432f889267a820407c39d
Jeongmo Seo <wjdah1402@gmail.com>
2025-12-04 20:42:48 +0900

    Modify Default classes - DefaultFailureResponseWriter - DefaultExceptionAdviceConfigurer

--- api-payload/src/main/java/org/namul/api/payload/error/DefaultExceptionAdviceConfigurer.java
+++ api-payload/src/main/java/org/namul/api/payload/error/DefaultExceptionAdviceConfigurer.java
@@ -8,15 +8,15 @@ public class DefaultExceptionAdviceConfigurer extends ExceptionAdviceConfigurer 
     public DefaultExceptionAdviceConfigurer(FailureResponseWriter failureResponseWriter) {
         super(failureResponseWriter);
         super
-                .addConstraintViolation(DefaultResponseErrorCode._CONSTRAINT_VIOLATION.getReason())
-                .addMethodArgumentNotValid(DefaultResponseErrorCode._METHOD_ARGUMENT_NOT_VALID.getReason())
-                .addHttpMessageNotReadable(DefaultResponseErrorCode._HTTP_MESSAGE_NOT_READABLE.getReason())
-                .addHttpRequestMethodNotSupported(DefaultResponseErrorCode._HTTP_REQUEST_METHOD_NOT_SUPPORTED.getReason())
-                .addMissingPathVariable(DefaultResponseErrorCode._MISSING_REQUEST_VALUE.getReason())
-                .addMissingServletRequestParameter(DefaultResponseErrorCode._MISSING_REQUEST_VALUE.getReason())
-                .addNoResourceFound(DefaultResponseErrorCode._NO_RESOURCE_FOUND.getReason())
-                .addTypeMismatch(DefaultResponseErrorCode._TYPE_MISMATCH.getReason())
-                .addServerApplication(DefaultResponseErrorCode._BAD_REQUEST.getReason())
-                .addGlobalException(DefaultResponseErrorCode._INTERNAL_SERVER_ERROR.getReason());
+                .addConstraintViolation(DefaultResponseErrorCode._CONSTRAINT_VIOLATION)
+                .addMethodArgumentNotValid(DefaultResponseErrorCode._METHOD_ARGUMENT_NOT_VALID)
+                .addHttpMessageNotReadable(DefaultResponseErrorCode._HTTP_MESSAGE_NOT_READABLE)
+                .addHttpRequestMethodNotSupported(DefaultResponseErrorCode._HTTP_REQUEST_METHOD_NOT_SUPPORTED)
+                .addMissingPathVariable(DefaultResponseErrorCode._MISSING_REQUEST_VALUE)
+                .addMissingServletRequestParameter(DefaultResponseErrorCode._MISSING_REQUEST_VALUE)
+                .addNoResourceFound(DefaultResponseErrorCode._NO_RESOURCE_FOUND)
+                .addTypeMismatch(DefaultResponseErrorCode._TYPE_MISMATCH)
+                .addServerApplication(DefaultResponseErrorCode._BAD_REQUEST)
+                .addGlobalException(DefaultResponseErrorCode._INTERNAL_SERVER_ERROR);
     }
 }
--- api-payload/src/main/java/org/namul/api/payload/writer/supports/DefaultFailureResponseWriter.java
+++ api-payload/src/main/java/org/namul/api/payload/writer/supports/DefaultFailureResponseWriter.java
@@ -2,14 +2,15 @@ package org.namul.api.payload.writer.supports;
 
 import org.namul.api.payload.code.dto.ErrorReasonDTO;
 import org.namul.api.payload.code.dto.supports.DefaultResponseErrorReasonDTO;
+import org.namul.api.payload.response.BaseResponse;
 import org.namul.api.payload.response.DefaultResponse;
 import org.namul.api.payload.writer.FailureResponseWriter;
 
 public class DefaultFailureResponseWriter implements FailureResponseWriter {
 
     @Override
-    public <T> DefaultResponse<T> onFailure(ErrorReasonDTO error, T result) {
+    public BaseResponse onFailure(Exception e, ErrorReasonDTO error) {
         DefaultResponseErrorReasonDTO dto = (DefaultResponseErrorReasonDTO) error;
-        return new DefaultResponse<>(false, dto.getCode(), dto.getMessage(), result);
+        return new DefaultResponse<>(false, dto.getCode(), dto.getMessage(), e.getMessage());
     }
 }
