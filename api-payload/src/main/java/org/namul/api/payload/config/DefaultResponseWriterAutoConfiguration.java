51ed59962581ea700fe5cda91fbc619bfe034424
Jeongmo Seo <wjdah1402@gmail.com>
2026-02-03 16:56:19 +0900

    Add SuccessResponseWriter for use when developers need to make success response

--- api-payload/src/main/java/org/namul/api/payload/config/DefaultResponseWriterAutoConfiguration.java
+++ api-payload/src/main/java/org/namul/api/payload/config/DefaultResponseWriterAutoConfiguration.java
@@ -1,14 +1,17 @@
 package org.namul.api.payload.config;
 
 import org.namul.api.payload.code.supports.DefaultBaseErrorCode;
+import org.namul.api.payload.code.supports.DefaultBaseSuccessCode;
 import org.namul.api.payload.writer.FailureResponseWriter;
+import org.namul.api.payload.writer.SuccessResponseWriter;
 import org.namul.api.payload.writer.supports.DefaultFailureResponseWriter;
+import org.namul.api.payload.writer.supports.DefaultSuccessResponseWriter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-public class DefaultFailureResponseWriterAutoConfiguration {
+public class DefaultResponseWriterAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(FailureResponseWriter.class)
@@ -16,4 +19,9 @@ public class DefaultFailureResponseWriterAutoConfiguration {
         return new DefaultFailureResponseWriter();
     }
 
+    @Bean
+    @ConditionalOnMissingBean(SuccessResponseWriter.class)
+    SuccessResponseWriter<DefaultBaseSuccessCode> defaultSuccessResponseWriter() {
+        return new DefaultSuccessResponseWriter();
+    }
 }
